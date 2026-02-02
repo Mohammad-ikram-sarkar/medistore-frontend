cc814491756632248d7bff29a790e0a1cadf8ef3
ikram <islamikram357@gmail.com>
2026-02-02 22:11:33 +0600

    problem fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
+import "./env";
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: `https://medistore-three.vercel.app/api/auth/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,17 +1,8 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  // baseURL: "" ,
-   baseURL : "http://localhost:8080",
-    //  baseURL : "https://medistore-three.vercel.app",
-   
-  // Ensure cookies are properly handled
-  // fetchOptions: {
-  //   credentials: 'include',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   // Add mode for CORS handling
-  //   mode: 'cors',
-  // },
-})
+  baseURL: typeof window !== "undefined" ? window.location.origin : "https://medistore-three.vercel.app",
+  fetchOptions: {
+    credentials: "include",
+  },
+});
